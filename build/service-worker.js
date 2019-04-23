0b55e44c093bec1fe24accde20d41dc9493899ec
aliu98 <amber.liu98@gmail.com>
2019-04-23 18:43:42 -0400

    update files

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.0c7222dbe21304e953c2a962f03d05c8.js"
 );
 
 workbox.clientsClaim();
